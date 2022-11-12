d3d240a3314db89109727d56d193b5b9d41fdf35
phpwyz <2012663060@qq.com>
2022-11-12 22:11:43 +0800

    add some files

---
+++ defer.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+const print = std.debug.print;
+
+var a: u8 = 1;
+
+pub fn main() !void
+{
+    _ = test();
+    print("{}", .{a});
+}
+
+fn test(a: u8) u8 {
+    //不知道
+    
+    defer a + 1;
+    return a;
+}
---
+++ undefined.zig
@@ -0,0 +1,9 @@
+//undefined
+
+pub fn main() !void
+{
+    var a: u8 = undefined;
+    var b: u8 = a;
+    var c: u8 = b;
+    _ = c;
+}
